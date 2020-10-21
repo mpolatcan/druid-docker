41bc3a19cc08e4e2ba42d3b0b454866aa957bd6b
Mutlu Polatcan <mutlupolatcan@gmail.com>
2020-10-22 01:36:25 +0300

    config load added to entrypoint

--- src/base/entrypoint.sh
+++ src/base/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+./druid_config_loader.sh
+
 for DRUID_SERVICE in ${DRUID_SERVICES[@]}; do
     echo "Starting service \"${DRUID_SERVICE}\"..."
     node.sh ${DRUID_SERVICE} start
 done
+
 
 tail -f /dev/null
